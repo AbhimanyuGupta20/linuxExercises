fe9356afc6fad224c869fbbb0dce647e8785d4d5
Peter Harmer <pharmer@wisc.edu>
2023-03-09 08:48:11 -0600

    five_dirs.sh completed, creates 4 files each in 5directories with 1 to 4 four lines each

--- five_dirs.sh
+++ five_dirs.sh
@@ -6,6 +6,10 @@ mkdir -p five/{dir1,dir2,dir3,dir4,dir5}
 count=1
 
 for d in five/*; do
-	echo $count >$d/file$count.txt
+	for ((i=1; i<=4; i++)) do
+		for ((j=1; j<=$i; j++)) do
+			echo $i >>$d/file$i.txt
+		done
+	done
 	count=$((count + 1))
 done
