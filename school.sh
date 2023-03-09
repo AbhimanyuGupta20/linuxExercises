13c7989522290a598b2a15d5b296823a8ef42ab8
Jonah <bostrom2@wisc.edu>
2023-03-09 09:54:53 -0600

    added school

--- school.sh
+++ school.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+head Property_Tax_Roll.csv | grep "MADISON SCHOOLS" Property_Tax_Roll.csv |cut -d',' -f7 Property_Tax_Roll.csv | awk '{sum += $1; count++} END {print "sum=", sum, "avg=", sum/count}'
