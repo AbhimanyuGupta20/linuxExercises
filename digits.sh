e1dcbe2542f2c9585a91147e24486c4d4b0d5d6d
Jonah <bostrom2@wisc.edu>
2023-03-09 09:53:11 -0600

    finally added digits.shcat digits.sh

--- digits.sh
+++ digits.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <dir> <n>"
+  exit 1
+fi
+
+dir=$1
+n=$2
+
+if [ ! -d $dir ]; then
+  echo "Error: $dir is not a directory."
+  exit 1
+fi
+
+cd $dir
+find . -maxdepth 1 -type f -size +${n}c -delete
+
